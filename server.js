a744332e6fc25abf6df7d61d42799f93a49908ba
Alec Hannaford <alec322@gmail.com>
2022-11-15 18:16:14 -0800

    display account total

--- server.js
+++ server.js
@@ -3,27 +3,40 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT;
 const Budget = require('./models/budget')
+const morgan = require('morgan')
+let account = 0
+
+
 
 ////////////////////////////////////////////?
 //Middleware
 ////////////////////////////////////////////?
 app.use("/static", express.static("public"))
+app.use(morgan('tiny'))
+app.use(express.urlencoded({extended:true})) //parse data from form submission
 
-
-//Show route for individual expenses
-app.get("/budgets/show/:id", (req,res) => {
-    res.render("show.ejs",{purchase:Budget[req.params.id]})
-})
 
 //Index route
 app.get("/budgets/", (req,res) => {
-    res.render("index.ejs",{Budget})
+    res.render("index.ejs",{Budget:Budget,account:account})
 })
 
 //NEW route
-app.get("/budgets/new",(req,res) => {
+app.get("/budgets/new", (req,res) =>{
     res.render("new.ejs")
 })
+
+//CREATE route
+app.post("/budgets/", (req,res) =>{
+    Budget.push(req.body)
+    res.redirect("/budgets/")
+})
+
+//Show route for individual purchases
+app.get("/budgets/:id", (req,res) => {
+    res.render("show.ejs",{purchase:Budget[req.params.id]})
+})
+
 
 //listeners
 app.listen(PORT, ()=>console.log(`Its go time! listening on ${PORT}`) )
